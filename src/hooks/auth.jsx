6c01a1fb22f57743b0b4d12e180553a850126aac
lucas <lucas327ks@gmail.com>
2023-04-20 15:15:46 -0300

    user session saved on localStorage

--- src/hooks/auth.jsx
+++ src/hooks/auth.jsx
@@ -1,19 +1,28 @@
-import { createContext, useContext } from "react"
+import { createContext, useContext, useState, useEffect } from "react"
 
 import { api } from "../services/index"
+
 
 const AuthContext = createContext({})
 
 function AuthProvider({ children }) {
 
+  const [data, setData] = useState({})
+
   async function signIn({ email, password }) {
 
     try {
       const response = await api.post("/sessions", { email, password })
-
       const { user, token } = response.data
-    }
-    catch (error) {
+      setData({ user, token })
+
+      localStorage.setItem("@rocketnotes:user", JSON.stringify(user))
+      localStorage.setItem("@rocketnotes:token", token)
+
+      api.defaults.headers.authorization = `Bearer ${token}`
+      // will pass the token through all api requests by default
+
+    } catch (error) {
       if (error.response) {
         alert(error.response.data.message)
       }
@@ -24,8 +33,25 @@ function AuthProvider({ children }) {
 
   }
 
+  useEffect(() => {
+
+    const user = localStorage.getItem("@rocketnotes:user")
+    const token = localStorage.getItem("@rocketnotes:token")
+
+    api.defaults.headers.authorization = `Bearer ${token}`
+
+    if (user && token) {
+      setData({
+        user: JSON.parse(user),
+        token
+      })
+    }
+
+  }, [])
+
   return (
-    <AuthContext.Provider value={{ signIn }}>
+    /* data that will be shared with the app in general */
+    <AuthContext.Provider value={{ signIn, user: data.user }}>
       {children}
     </AuthContext.Provider>
     /* context.Provider provides a default value to be used when is requested */
